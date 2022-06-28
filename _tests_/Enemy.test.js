bc6c6d630d21da8400c6b93269ce32f5318d74de
agvan <agvandyke25@gmail.com>
2022-06-28 09:57:54 -0500

    added require statements & failing test for enemy object

---
+++ _tests_/Enemy.test.js
@@ -0,0 +1,18 @@
+// require statements
+
+const Enemy = require('../lib/Enemy.js');
+const Potion = require('../lib/Potion.js');
+
+jest.mock('../lib/Potion.js');
+
+//enemy object test
+test('creates an enemy object', () => {
+    const enemy = new Enemy('goblin', 'sword');
+
+    expect(enemy.name).toBe('goblin');
+    expect(enemy.weapon).toBe('sword');
+    expect(enemy.health).toEqual(expect.any(Number));
+    expect(enemy.strength).toEqual(expect.any(Number));
+    expect(enemy.agility).toEqual(expect.any(Number));
+    expect(enemy.potion).toEqual(expect.any(Object));
+});
